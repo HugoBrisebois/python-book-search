a823b6ae986ecd015f3596fac7c393b0561d14b8
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2025-07-10 21:05:48 -0400

    finished the landing image and text

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+# Arriving program
+Home_message = """ _____ _           _                            _                 _    
+|  ___(_)_ __   __| |  _   _  ___  _   _ _ __  | |__   ___   ___ | | __
+| |_  | | '_ \ / _` | | | | |/ _ \| | | | '__| | '_ \ / _ \ / _ \| |/ /
+|  _| | | | | | (_| | | |_| | (_) | |_| | |    | |_) | (_) | (_) |   < 
+|_|   |_|_| |_|\__,_|  \__, |\___/ \__,_|_|    |_.__/ \___/ \___/|_|\_\
+                                    |___/                                           """
+
+print(Home_message)
+
+# search question
+
+print("What Book are you searching for?")
+book = input()
+
